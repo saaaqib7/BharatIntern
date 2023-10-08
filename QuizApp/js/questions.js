e207126576dbeefdb2e320a8eb8529d8124edbaf
Syed Saqib <121810079+saaaqib7@users.noreply.github.com>
2023-10-08 23:16:02 +0530

    Add files via upload

---
+++ QuizApp/js/questions.js
@@ -0,0 +1,125 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What is the result of 5 + 3 * 2?",
+    answer: " 11",
+    options: [
+      "10",
+      "11",
+      "16",
+      "5 + 3 * 2"
+
+
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What does HTML stand for?",
+    answer: "HyperText Markup Language",
+    options: [
+      "HyperText Markup Language",
+      "High-Throughput Multi-Language",
+      "Hyper Transfer Markup Logic",
+      "Hypertext Technical Markup"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which programming language is often used for developing mobile applications",
+    answer: "Java",
+    options: [
+      " JavaScript",
+      "C#",
+      "Ruby",
+      "Java"
+    ]
+  },
+  {
+    numb: 8,
+    question: "In Python, which keyword is used to define a function?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "def",
+      "function",
+      "define",
+      "func"
+    ]
+  },
+  {
+    numb: 9,
+    question: "In JavaScript, which of the following is used to declare a variable?",
+    answer: "var",
+    options: [
+      "v",
+      "variable",
+      "var",
+      "let"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What does the acronym API stand for in the context of web development?",
+    answer: " Functional",
+    options: [
+      "Object-Oriented",
+      "Functional",
+      "Imperative",
+      "Procedural"
+    ]
+  },
+];
